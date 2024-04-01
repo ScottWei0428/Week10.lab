88dc62c9aa7a6610811f1587f3fd60b173994e41
YuchenWei <scott.wei0428@gmail.com>
2024-04-01 13:20:25 -0400

    Add project files.

---
+++ Models/Entities/OrderOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week10.lab.Models.Entities;
+
+public partial class OrderOption
+{
+    public decimal SalesTaxRate { get; set; }
+
+    public decimal FirstBookShipCharge { get; set; }
+
+    public decimal AdditionalBookShipCharge { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Week10.lab.Models.Entities;
+var dbContext = new MMABooksContext();
+var customers = dbContext.Customers;
+foreach (var customer in customers)
+{
+    Console.WriteLine(customer.Name);
+}
